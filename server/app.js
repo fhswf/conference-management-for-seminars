f07b95d0870cdac2240cad1cf16372329f08aaf4
Oliver Sabelfeld <o.sd@tutanota.de>
2023-11-29 16:37:56 +0100

    invalid signature error fixed

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const fs = require('fs');
 
 
 const app = express();
-
+app.set('trust proxy', true);
 // ------------------------------ middleware ------------------------------
 app.use(cors({
     origin: process.env.FRONTEND_URL,
