cdb722d8a622c7f9ac7a26877db4df212afc64cd
Oliver Sabelfeld <o.sd@tutanota.de>
2023-12-05 22:22:53 +0100

    Update passport.js

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,102 +1,170 @@
 const passport = require('passport');
 const LTIStrategy = require('passport-lti');
+const OpenIDConnectStrategy = require('passport-openidconnect');
 
 const db = require("../models");
-const Person = db.person;
-const RoleAssignment = db.rolleassignment;
+const crypto = require("crypto");
+const User = db.user;
+const RoleAssignment = db.roleassignment;
 const Seminar = db.seminar;
+const OidcUser = db.oidcuser;
+const LtiUser = db.ltiuser
+const ContextToSeminar = db.contexttoseminar;
 
-async function addOrUpdatePerson(lti, t) {
-    const [person, created] = await Person.findOrCreate({
+async function addOrUpdateUser(lti, t) {
+    let ltiUser = await LtiUser.findOne({
         where: {
-            personOID: lti.user_id
+            LtiUserOID: lti.user_id,
+            consumerURL: lti.tool_consumer_instance_guid
+        }
+    });
+
+    if (!ltiUser) {
+        // Create a new User
+        const user = await User.create({
+            firstName: lti.lis_person_name_given,
+            lastName: lti.lis_person_name_family,
+            mail: lti.lis_person_contact_email_primary,
+            isAdmin: false,
+        }, {transaction: t});
+
+        // Create LtiUser
+        ltiUser = await LtiUser.create({
+            LtiUserOID: lti.user_id,
+            consumerURL: lti.tool_consumer_instance_guid,
+            userOID: user.userOID,
+        }, {transaction: t});
+    }
+    // only the User table should be updated
+    const [user, created] = await User.findOrCreate({
+        where: {
+            userOID: ltiUser.userOID
         },
         defaults: {
-            personOID: lti.user_id,
             firstName: lti.lis_person_name_given,
             lastName: lti.lis_person_name_family,
             mail: lti.lis_person_contact_email_primary,
             comment: null,
-            passwort: ""
-        },
-        transaction: t
-    })
-
-    if (!created) {
-        person.firstName = lti.lis_person_name_given;
-        person.lastName = lti.lis_person_name_family;
-        person.mail = lti.lis_person_contact_email_primary;
-        person.comment = "updated2";
-        person.passwort = "";
-        await person.save({transaction: t});
-    }
-
-    return person;
-}
-
-async function addOrUpdateSeminar(lti, t) {
-    const [seminar, created] = await Seminar.findOrCreate({
-        where: {
-            seminarOID: lti.context_id
-        },
-        defaults: {
-            seminarOID: lti.context_id,
-            description: lti.context_title,
-            phase: 1
-        },
-        transaction: t
+            isAdmin: false,
+        }, transaction: t
     });
 
     if (!created) {
-        seminar.description = lti.context_title;
-        //seminar.phase = 1;
-        await seminar.save({transaction: t});
+        // update user
+        user.firstName = lti.lis_person_name_given;
+        user.lastName = lti.lis_person_name_family;
+        user.mail = lti.lis_person_contact_email_primary;
+        await user.save({transaction: t});
     }
+
+    return user;
+}
+
+async function addSeminar(lti, t) {
+    const key = crypto.randomUUID()
+
+    const map = await ContextToSeminar.findOne({
+        where: {
+            LtiContextId: lti.context_id,
+            consumerURL: lti.tool_consumer_instance_guid
+        }
+    });
+
+    if(!map){
+        //1. erstelle seminar
+        const seminar = await Seminar.create({
+            description: lti.context_title,
+            phase: 1,
+            assignmentkey: key
+        } , {transaction: t});
+
+        //2. erstelle mapping
+        const mapping = await ContextToSeminar.create({
+            LtiContextId: lti.context_id,
+            consumerURL: lti.tool_consumer_instance_guid,
+            seminarOID: seminar.seminarOID
+        }, {transaction: t});
+
+        return seminar;
+    }
+
+    const seminar = await Seminar.findOne({
+        where: {
+            seminarOID: map.seminarOID
+        }
+    });
+
+    // TODO delete
+    // seminar.description = lti.context_title;
+    // //seminar.phase = 1;
+    // await seminar.save({transaction: t});
+
     return seminar;
 }
 
-async function addOrUpdateRolleAssignment(lti, t) {
+// TODO
+function getUserIdfromLti(lti) {
+
+}
+
+async function addRoleAssignment(lti, user, seminar, t) {
     const [assignment, created] = await RoleAssignment.findOrCreate({
         where: {
-            personOID: lti.user_id,
-            seminarOID: lti.context_id
+            userOID: user.userOID,
+            seminarOID: seminar.seminarOID
         },
         defaults: {
-            personOID: lti.user_id,
-            seminarOID: lti.context_id,
-            roleOID: mapRole(lti.roles)
+            userOID: user.userOID,
+            seminarOID: seminar.seminarOID,
+            roleOID: mapLtiRoles(lti.roles)
         },
         transaction: t
     });
 
-    if (!created) {
-        // TODO if role changed, handle uploaded User data (paper, concept)
-        assignment.role = mapRole(lti.roles);
-        await assignment.save({transaction: t});
-    }
     return assignment;
 }
 
-function mapRole(roles) {
+function mapLtiRoles(roles) {
     //if(roles[0] === 'Admin') return 1; //moodle role does not exist
     if (roles[0] === 'Instructor') return 2;
     if (roles[0] === 'Learner') return 3;
     return null;
 }
 
-const verifyCallback = async (username, lti, done) => {
+const ltiVerifyCallback = async (username, lti, done) => {
     const t = await db.sequelize.transaction();
 
     try {
-        const person = await addOrUpdatePerson(lti, t);
-        await addOrUpdateSeminar(lti, t);
-        await addOrUpdateRolleAssignment(lti, t);
+        const user = await addOrUpdateUser(lti, t);
 
-        var user = {id: person.personOID, lti: lti};
+        let seminar = null;
+        if (lti.custom_seminar_key) {
+            //join seminar
+            seminar = await Seminar.findOne({
+                where: {
+                    key: lti.custom_seminar_key
+                }
+            });
+            if (!seminar) {
+                console.log("invalid seminar key")
+                return done(null, false);
+            }
+        } else {
+            //create seminar
+            seminar = await addSeminar(lti, t);
+        }
+
+        await addRoleAssignment(lti, user, seminar, t); // TODO only if phase 1 ?
+
+        var userJson = {
+            id: user.userOID,
+            lti: lti,
+            authtype: "lti"
+        };
 
         await t.commit();
 
-        return done(null, user);
+        return done(null, userJson);
     } catch (e) {
         console.log(e);
         await t.rollback();
@@ -104,28 +172,124 @@ const verifyCallback = async (username, lti, done) => {
     }
 }
 
-
-const strategy = new LTIStrategy({
+const ltiStrategy = new LTIStrategy({
     consumerKey: process.env.CONSUMER_KEY,
-    consumerSecret: process.env.CONSUMER_SECRET ,
+    consumerSecret: process.env.CONSUMER_SECRET,
     passReqToCallback: true,
-}, verifyCallback);
+}, ltiVerifyCallback);
 
-passport.use(strategy);
+async function oidcVerifyCallback(issuer, profile, context, idToken, accessToken, refreshToken, params, done) {
+    const t = await db.sequelize.transaction();
+
+    try {
+        let cred = await OidcUser.findOne({
+            where: {
+                subject: profile.id
+            }
+        });
+
+        if (!cred) {
+            // Create a new User
+            const user = await User.create({
+                firstName: profile.name?.givenName || "",
+                lastName: profile.name?.familyName || "",
+                mail: profile.emails && profile.emails[0]?.value || "",
+                comment: null,
+                isAdmin: false,
+            }, {transaction: t});
+
+            // Create OidcUser
+            cred = await OidcUser.create({
+                subject: profile.id,
+                provider: issuer,
+                userOID: user.userOID,
+                idToken: idToken,
+            }, {transaction: t});
+        }
+        // only the User table should be updated
+        const [user, created] = await User.findOrCreate({
+            where: {
+                userOID: cred.userOID
+            },
+            defaults: {
+                firstName: profile.name?.givenName || "",
+                lastName: profile.name?.familyName || "",
+                mail: profile.emails && profile.emails[0]?.value || "",
+                comment: null,
+                isAdmin: false,
+            }, transaction: t
+        });
+
+        if (!created) {
+            // update user
+            user.firstName = profile.name?.givenName || "";
+            user.lastName = profile.name?.familyName || "";
+            user.mail = profile.emails && profile.emails[0]?.value || "";
+            await user.save({transaction: t});
+        }
+
+        if (!user) {
+            return done(null, false);
+        }
+
+        await t.commit();
+
+        return done(null, {
+            id: user.userOID,
+            lti: null,
+            authtype: "oidc",
+            accessToken: accessToken,
+            refreshToken: refreshToken,
+            idToken: idToken
+        });
+    } catch (err) {
+        await t.rollback();
+        return done(err);
+    }
+}
+
+
+passport.use(ltiStrategy);
+
+passport.use(new OpenIDConnectStrategy({
+    issuer: process.env.ISSUER,
+    authorizationURL: process.env.AUTHORIZATION_URL,
+    tokenURL: process.env.TOKEN_URL,
+    userInfoURL: process.env.USERINFO_URL,
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL
+}, oidcVerifyCallback));
+
 
 passport.serializeUser((user, done) => {
     done(null, {
-        personOID: user.id,
+        userOID: user.id,
         lti: user.lti,
+        authtype: user.authtype,
+        accessToken: user.accessToken,
+        refreshToken: user.refreshToken,
+        idToken: user.idToken
     });
 });
 
 passport.deserializeUser((serializedUser, done) => {
-    Person.findByPk(serializedUser.personOID)
+    User.findByPk(serializedUser.userOID)
         .then((user) => {
             if (user) {
-                user.lti = serializedUser.lti;
-                done(null, user);
+                const userJson = {}
+                userJson.userOID = serializedUser.userOID;
+                userJson.lti = serializedUser.lti;
+                userJson.authtype = serializedUser.authtype;
+                userJson.accessToken = serializedUser.accessToken;
+                userJson.refreshToken = serializedUser.refreshToken;
+                userJson.idToken = serializedUser.idToken;
+
+                userJson.mail = user.mail;
+                userJson.isAdmin = user.isAdmin;
+                userJson.firstName = user.firstName;
+                userJson.lastName = user.lastName;
+                done(null, userJson);
             } else {
                 done(null, false);
             }
